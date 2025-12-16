5a1592ec40b118fc9ee31c0fdf8a54318562bc2c
Pulpyyyy <32004297+Pulpyyyy@users.noreply.github.com>
2025-12-16 17:28:07 +0100

    cleanup

--- apps/schedule_parser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Schedule Parser for Home Assistant."""
-
-DOMAIN = "schedule_parser"
-PLATFORMS = []
-
-async def async_setup(hass, config):
-    """Set up the Schedule Parser component."""
-    return True
